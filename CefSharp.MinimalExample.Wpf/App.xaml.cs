bb6de7dc3643bccec56a773404b2a226e5e15c1c
Smirnov Denis <dsmirnov@marathonbet.ru>
2016-03-17 15:44:41 +0300

    init Cef in splash screen to show not closing app errro

--- CefSharp.MinimalExample.Wpf/App.xaml.cs
+++ CefSharp.MinimalExample.Wpf/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace CefSharp.MinimalExample.Wpf
 {
@@ -6,10 +8,27 @@ namespace CefSharp.MinimalExample.Wpf
     {
         public App()
         {
-            //Perform dependency check to make sure all relevant resources are in our output directory.
-            var settings = new CefSettings();
-            settings.EnableInternalPdfViewerOffScreen();
-            Cef.Initialize(settings, shutdownOnProcessExit: true, performDependencyCheck: true);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen();
+            splashScreen.AppInitilizationDone += SplashScreenOnAppInitilizationDone;
+            //MainWindow = splashScreen;
+            splashScreen.Show();
+
+            base.OnStartup(e);
+        }
+
+        private void SplashScreenOnAppInitilizationDone(object sender, EventArgs eventArgs)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                var splashScreen = MainWindow;
+                MainWindow = new MainWindow();
+                splashScreen.Close();
+                MainWindow.Show();
+            }));
         }
     }
 }
